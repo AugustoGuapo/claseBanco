93ebe37030356d5ef02c7eb9384740abd7fe7404
Augusto Sergio Diaz Polo <adiazpolo17@correo.unicordoba.edu.co>
2022-04-10 15:37:59 -0500

    Creación del repositorio claseBanco

---
+++ src/clasebanco/modelo.java
@@ -0,0 +1,118 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package clasebanco;
+
+/**
+ *
+ * @author user
+ */
+public class modelo {
+    private String nombre;
+    private String fechaCreacion;
+    private String numeroTelefono;
+    private String sexo;
+    private String tipoDeCuenta;
+    private String numeroDeCuenta;
+    private int saldo;
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the fechaCreacion
+     */
+    public String getFechaCreacion() {
+        return fechaCreacion;
+    }
+
+    /**
+     * @param fechaCreacion the fechaCreacion to set
+     */
+    public void setFechaCreacion(String fechaCreacion) {
+        this.fechaCreacion = fechaCreacion;
+    }
+
+    /**
+     * @return the numeroTelefono
+     */
+    public String getNumeroTelefono() {
+        return numeroTelefono;
+    }
+
+    /**
+     * @param numeroTelefono the numeroTelefono to set
+     */
+    public void setNumeroTelefono(String numeroTelefono) {
+        this.numeroTelefono = numeroTelefono;
+    }
+
+    /**
+     * @return the sexo
+     */
+    public String getSexo() {
+        return sexo;
+    }
+
+    /**
+     * @param sexo the sexo to set
+     */
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    /**
+     * @return the tipoDeCuenta
+     */
+    public String getTipoDeCuenta() {
+        return tipoDeCuenta;
+    }
+
+    /**
+     * @param tipoDeCuenta the tipoDeCuenta to set
+     */
+    public void setTipoDeCuenta(String tipoDeCuenta) {
+        this.tipoDeCuenta = tipoDeCuenta;
+    }
+
+    /**
+     * @return the numeroDeCuenta
+     */
+    public String getNumeroDeCuenta() {
+        return numeroDeCuenta;
+    }
+
+    /**
+     * @param numeroDeCuenta the numeroDeCuenta to set
+     */
+    public void setNumeroDeCuenta(String numeroDeCuenta) {
+        this.numeroDeCuenta = numeroDeCuenta;
+    }
+
+    /**
+     * @return the saldo
+     */
+    public int getSaldo() {
+        return saldo;
+    }
+
+    /**
+     * @param saldo the saldo to set
+     */
+    public void setSaldo(int saldo) {
+        this.saldo += saldo;
+    }
+    
+}
